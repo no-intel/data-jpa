fb2d4983ad942a1a5490ff4193de4aa1fbfffab9
tkdduf212@gmail.com <tkdduf212@gmail.com>
2024-03-15 02:55:38 +0900

    페이징

--- src/main/java/org/jpabook/datajpa/controller/MemberController.java
+++ src/main/java/org/jpabook/datajpa/controller/MemberController.java
@@ -4,6 +4,9 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.jpabook.datajpa.entity.Member;
 import org.jpabook.datajpa.repository.MemberRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +19,9 @@ public class MemberController {
 
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user"+ i, i));
+        }
     }
 
     @GetMapping("/members/{id}")
@@ -28,6 +33,11 @@ public class MemberController {
     @GetMapping("/members2/{id}")
     public String findMember2(@PathVariable("id") Member member) {
         return member.getUsername();
+    }
+
+    @GetMapping("/members")
+    public Page<Member> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable);
     }
 
 }
